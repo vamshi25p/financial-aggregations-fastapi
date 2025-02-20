3480603a300083ddc45cb64d510b65fe81a16838
Vamshi Krishna <vamshikrishna788sai@gmail.com>
2025-02-20 16:35:39 +0530

    refactor rating generation logic; extract categorization functions for credit score, credit limit, and turnover

--- backend/repository/processfile.py
+++ backend/repository/processfile.py
@@ -9,31 +9,48 @@ file_path = os.path.join(data_dir, '20K Financial Data.csv')
 def load_data():
     return pl.scan_csv(file_path, ignore_errors=True)
 
+def categorize_credit_score(x):
+    if x >= 85:
+        return "A"
+    elif 50 <= x < 85:
+        return "B"
+    elif 35 <= x < 50:
+        return "C"
+    elif 0 <= x < 35:
+        return "D"
+    return "E"
+
+def categorize_credit_limit(x):
+    if x >= 250000:
+        return "A"
+    elif 100000 <= x < 250000:
+        return "B"
+    elif 50000 <= x < 100000:
+        return "C"
+    elif x < 50000:
+        return "D"
+    return "E"
+
+
+def categorize_turnover(x):
+    return categorize_credit_limit(x)
+
 def generate_rating_json(data_lf):
     rating_lf = data_lf.select([
         pl.col("cs_company_id").alias("safeNumber"),
         
         pl.col("credit_score").map_elements(
-            lambda x: "A" if x >= 85 else
-                      "B" if 50 <= x < 85 else
-                      "C" if 35 <= x < 50 else
-                      "D" if 0 <= x < 35 else "E",
+            categorize_credit_score,
             return_dtype=pl.String
         ).alias("credit_score_type"),
 
         pl.col("credit_limit").map_elements(
-            lambda x: "A" if x >= 250000 else
-                      "B" if 100000 <= x < 250000 else
-                      "C" if 50000 <= x < 100000 else
-                      "D" if x < 50000 else "E",
+            categorize_credit_limit,
             return_dtype=pl.String
         ).alias("credit_limit_type"),
 
         pl.col("credit_limit").map_elements(
-            lambda x: "A" if x >= 250000 else
-                      "B" if 100000 <= x < 250000 else
-                      "C" if 50000 <= x < 100000 else
-                      "D" if x < 50000 else "E",
+            categorize_turnover,
             return_dtype=pl.String
         ).alias("turnover_type"),
 
